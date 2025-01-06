0f5fc973de6d0f8126498276f62c9868582e0195
Sumit <114601477+kvinaaaaykumar@users.noreply.github.com>
2025-01-06 07:39:50 +0530

    Update App.jsx
    s

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const link = "http://localhost:8080"
+const link = "https://sooooooooo.onrender.com"
 
 
 const App = () => {
